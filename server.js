a6185a0e34b3c58d6e2db0cb157b82f7f059f513
Austin Henrie <nitsua15@gmail.com>
2023-07-09 22:01:34 -0600

    implemented deletion

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let database = new sequelize(require("./secrets").key, {
 let schema = {
     "users": database.define("users", { "name": sequelize.DataTypes.TEXT, "password": sequelize.DataTypes.TEXT }),
     "shares": database.define("shares", { "user": sequelize.DataTypes.TEXT, "item": sequelize.DataTypes.DECIMAL }),
-    "items": database.define("items", { "owner": sequelize.DataTypes.DECIMAL, "parent": sequelize.DataTypes.DECIMAL, "order": sequelize.DataTypes.DECIMAL, "name": sequelize.DataTypes.TEXT, "description": sequelize.DataTypes.TEXT, "address": sequelize.DataTypes.TEXT, "public": sequelize.DataTypes.BOOLEAN, "shortcut": sequelize.DataTypes.DECIMAL }),
+    "items": database.define("items", { "index": sequelize.DataTypes.DECIMAL, "owner": sequelize.DataTypes.DECIMAL, "parent": sequelize.DataTypes.DECIMAL, "order": sequelize.DataTypes.DECIMAL, "name": sequelize.DataTypes.TEXT, "description": sequelize.DataTypes.TEXT, "address": sequelize.DataTypes.TEXT, "public": sequelize.DataTypes.BOOLEAN, "shortcut": sequelize.DataTypes.DECIMAL }),
     "tags": database.define("tags", { "name": sequelize.DataTypes.TEXT, "item": sequelize.DataTypes.DECIMAL })
 }
 let api = express()
@@ -19,6 +19,11 @@ api.get("/item", async function(request, response) {
 })
 api.post("/item", function(request) {
     schema.items.create(request.body)
+})
+api.delete("/item/:index", function(request) {
+    schema.items.destroy({
+        "where": { "index": Number(request.params.index) }
+    })
 })
 api.use(express.static(__dirname + "/build"))
 database.sync().then(function() {
--- src/index.js
+++ src/index.js
@@ -2,25 +2,34 @@ import react from "react"
 import reactDOM from "react-dom/client"
 import axios from "axios"
 let items
-function Bookmarks() {
+function Main() {
   let renderer = react.useState()[1]
   function render() {
     renderer({})
   }
-  let name = react.useRef()
   let address = react.useRef()
+  let name = react.useRef()
   let description = react.useRef()
   let list = []
   for (let index in items) {
-    list.push(<a key = {index} target = {"_blank"} href = {items[index].address} style = {{ "display": "flex", "borderStyle": "solid", "textDecoration": "none", "color": "black" }}>
-      <img src = {"https://www.google.com/s2/favicons?domain=" + items[index].address} />
-      <div style = {{ "fontWeight": "bold" }}>
-        {items[index].name}
-      </div>
-      <div>
-        {items[index].description}
-      </div>
-    </a>)
+    list.push(<div key = {index} style = {{ "borderStyle": "solid", "display": "flex", "justifyContent": "space-between" }}>
+      <a target = {"_blank"} href = {items[index].address} style = {{ "display": "flex", "alignItems": "center", "textDecoration": "none", "color": "black" }}>
+        <img src = {"https://www.google.com/s2/favicons?domain=" + items[index].address} style = {{ "height": "16px", "width": "16px" }} />
+        <div style = {{ "fontWeight": "bold" }}>
+          {items[index].name}
+        </div>
+        <div>
+          {items[index].description}
+        </div>
+      </a>
+      <button onClick = {function() {
+        axios.delete("/item/" + items[index].index)
+        delete items[index]
+        render()
+      }} style = {{ "margin": "16px" }}>
+        delete
+      </button>
+    </div>)
   }
   return <>
     <div style = {{ "borderStyle": "solid", "display": "flex", "flexDirection": "column" }}>
@@ -28,10 +37,19 @@ function Bookmarks() {
     </div>
     <form onSubmit = {function(event) {
       event.preventDefault()
-      axios.post("/item", { "name": name.current.value, "address": address.current.value, "description": description.current.value })
-      items.push({ "name": name.current.value, "address": address.current.value, "description": description.current.value })
-      name.current.value = ""
+      let index
+      if (items[0]) {
+        index = items[items.length - 1].index + 1
+      } else {
+        index = 0
+      }
+      if (!(address.current.value.startsWith("https://") || address.current.value.startsWith("http://"))) {
+        address.current.value = "https://" + address.current.value
+      }
+      axios.post("/item", { "index": index, "name": name.current.value, "address": address.current.value, "description": description.current.value })
+      items.push({ "index": index, "name": name.current.value, "address": address.current.value, "description": description.current.value })
       address.current.value = ""
+      name.current.value = ""
       description.current.value = ""
       render()
     }}>
@@ -47,6 +65,6 @@ function Bookmarks() {
 axios.get("/item").then(function(response) {
   items = response.data
   reactDOM.createRoot(document.querySelector("div")).render(<react.StrictMode>
-    <Bookmarks />
+    <Main />
   </react.StrictMode>)
 })
