917a9f1aee7bfad593a0b317eacc502f246d5126
Austin Henrie <nitsua15@gmail.com>
2023-07-09 18:25:30 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+let sequelize = require("sequelize")
+let express = require("express")
+let database = new sequelize(require("./secrets").key, {
+    "dialect": "postgres",
+    "dialectOptions": {
+        "ssl": { "rejectUnauthorized": false }
+    }
+})
+let schema = {
+    "users": database.define("users", { "name": sequelize.DataTypes.TEXT, "password": sequelize.DataTypes.TEXT }),
+    "shares": database.define("shares", { "user": sequelize.DataTypes.TEXT, "item": sequelize.DataTypes.DECIMAL }),
+    "items": database.define("items", { "owner": sequelize.DataTypes.DECIMAL, "parent": sequelize.DataTypes.DECIMAL, "order": sequelize.DataTypes.DECIMAL, "name": sequelize.DataTypes.TEXT, "description": sequelize.DataTypes.TEXT, "address": sequelize.DataTypes.TEXT, "public": sequelize.DataTypes.BOOLEAN, "shortcut": sequelize.DataTypes.DECIMAL }),
+    "tags": database.define("tags", { "name": sequelize.DataTypes.TEXT, "item": sequelize.DataTypes.DECIMAL })
+}
+let api = express()
+api.use(express.json())
+api.get("/item", async function(request, response) {
+    response.send(await schema.items.findAll())
+})
+api.post("/item", function(request) {
+    schema.items.create(request.body)
+})
+api.use(express.static(__dirname + "/build"))
+database.sync().then(function() {
+    api.listen(3000)
+})
--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import react from "react"
+import reactDOM from "react-dom/client"
+import axios from "axios"
+let items
+function Bookmarks() {
+  let renderer = react.useState()[1]
+  function render() {
+    renderer({})
+  }
+  let name = react.useRef()
+  let address = react.useRef()
+  let description = react.useRef()
+  let list = []
+  for (let index in items) {
+    list.push(<a key = {index} target = {"_blank"} href = {items[index].address} style = {{ "display": "flex", "borderStyle": "solid", "textDecoration": "none", "color": "black" }}>
+      <img src = {"https://www.google.com/s2/favicons?domain=" + items[index].address} />
+      <div style = {{ "fontWeight": "bold" }}>
+        {items[index].name}
+      </div>
+      <div>
+        {items[index].description}
+      </div>
+    </a>)
+  }
+  return <>
+    <div style = {{ "borderStyle": "solid", "display": "flex", "flexDirection": "column" }}>
+      {list}
+    </div>
+    <form onSubmit = {function(event) {
+      event.preventDefault()
+      axios.post("/item", { "name": name.current.value, "address": address.current.value, "description": description.current.value })
+      items.push({ "name": name.current.value, "address": address.current.value, "description": description.current.value })
+      name.current.value = ""
+      address.current.value = ""
+      description.current.value = ""
+      render()
+    }}>
+      <input ref = {address} placeholder = {"address"} />
+      <input ref = {name} placeholder = {"name"} />
+      <input ref = {description} placeholder = {"description"} />
+      <button>
+        add
+      </button>
+    </form>
+  </>
+}
+axios.get("/item").then(function(response) {
+  items = response.data
+  reactDOM.createRoot(document.querySelector("div")).render(<react.StrictMode>
+    <Bookmarks />
+  </react.StrictMode>)
+})
