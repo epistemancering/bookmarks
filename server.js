968f919637986d043d4cdc3f2a24a8d7a26755ef
Austin Henrie <nitsua15@gmail.com>
2023-07-13 22:19:59 -0600

    implemented navigation bar

--- server.js
+++ server.js
@@ -6,32 +6,33 @@ let database = new sequelize(require("./secrets").key, {
         "ssl": { "rejectUnauthorized": false }
     }
 })
-let schema = {
-    "users": database.define("users", { "name": sequelize.DataTypes.TEXT, "password": sequelize.DataTypes.TEXT }),
-    "shares": database.define("shares", { "user": sequelize.DataTypes.TEXT, "item": sequelize.DataTypes.DECIMAL }),
-    "items": database.define("items", { "user": sequelize.DataTypes.TEXT, "index": sequelize.DataTypes.DECIMAL, "parent": sequelize.DataTypes.DECIMAL, "order": sequelize.DataTypes.DECIMAL, "name": sequelize.DataTypes.TEXT, "description": sequelize.DataTypes.TEXT, "address": sequelize.DataTypes.TEXT, "public": sequelize.DataTypes.BOOLEAN, "shortcut": sequelize.DataTypes.DECIMAL }),
-    "tags": database.define("tags", { "name": sequelize.DataTypes.TEXT, "item": sequelize.DataTypes.DECIMAL })
-}
+let users = database.define("users", { "name": sequelize.DataTypes.TEXT, "password": sequelize.DataTypes.TEXT })
+let items = database.define("items", { "user": sequelize.DataTypes.TEXT, "index": sequelize.DataTypes.DECIMAL, "parent": sequelize.DataTypes.DECIMAL, "order": sequelize.DataTypes.DECIMAL, "name": sequelize.DataTypes.TEXT, "description": sequelize.DataTypes.TEXT, "address": sequelize.DataTypes.TEXT, "public": sequelize.DataTypes.BOOLEAN, "shortcut": sequelize.DataTypes.DECIMAL })
+let tags = database.define("tags", { "name": sequelize.DataTypes.TEXT, "item": sequelize.DataTypes.DECIMAL })
+let shares = database.define("shares", { "user": sequelize.DataTypes.TEXT, "item": sequelize.DataTypes.DECIMAL })
 let api = express()
 api.use(express.json())
-api.get("/item/:user", function(request, response) {
-    schema.items.findAll({
+api.post("/findAll", function(request, response) {
+    items.findAll({
         "order": ["index"],
-        "where": { "user": request.params.user }
+        "where": { "user": request.body.user }
     }).then(function(selection) {
         response.send(selection)
     })
 })
-api.post("/item", function(request) {
-    schema.items.create(request.body)
+api.post("/create", function(request, response) {
+    response.sendStatus(200)
+    items.create(request.body)
 })
-api.put("/item", function(request) {
-    schema.items.update(request.body, {
+api.put("/update", function(request, response) {
+    response.sendStatus(200)
+    items.update(request.body, {
         "where": { "user": request.body.user, "index": request.body.index }
     })
 })
-api.put("/destroy", function(request) {
-    schema.items.destroy({
+api.put("/destroy", function(request, response) {
+    response.sendStatus(200)
+    items.destroy({
         "where": { "user": request.body.user, "index": request.body.destroyed }
     })
 })
--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ let path = decodeURI(window.location.pathname).split("/")
 let user = path[1]
 let map = []
 let step = 2
-let parent = "0"
-let redirect = user
 let items = [[]]
 let next
 let state = {}
 let destroyed
+window.addEventListener("popstate", function() {
+  render("Main")
+})
 function render(component) {
   state[component][1]({})
 }
@@ -36,28 +37,28 @@ function Bookmark(props) {
       if (!(address.current.value.startsWith("https://") || address.current.value.startsWith("http://"))) {
         address.current.value = "https://" + address.current.value
       }
-      items[parent][props.index].address = address.current.value
-      items[parent][props.index].name = name.current.value
-      items[parent][props.index].description = description.current.value
-      axios.put("/update", items[parent][props.index])
+      items[window.history.state.current][props.index].address = address.current.value
+      items[window.history.state.current][props.index].name = name.current.value
+      items[window.history.state.current][props.index].description = description.current.value
+      axios.put("/update", items[window.history.state.current][props.index])
       editing[1]()
     }}>
-      <input ref = {address} defaultValue = {items[parent][props.index].address} placeholder = {"address"} />
-      <input ref = {name} defaultValue = {items[parent][props.index].name} placeholder = {"name"} />
-      <input ref = {description} defaultValue = {items[parent][props.index].description} placeholder = {"description"} />
+      <input ref = {address} defaultValue = {items[window.history.state.current][props.index].address} placeholder = {"address"} />
+      <input ref = {name} defaultValue = {items[window.history.state.current][props.index].name} placeholder = {"name"} />
+      <input ref = {description} defaultValue = {items[window.history.state.current][props.index].description} placeholder = {"description"} />
       <button>
         done
       </button>
     </form>
   }
   return <>
-    <a className = {"item"} target = {"_blank"} href = {items[parent][props.index].address} style = {{ "textDecoration": "none", "color": "black" }}>
-      <img src = {"https://www.google.com/s2/favicons?domain=" + items[parent][props.index].address} style = {{ "height": "16px", "width": "16px" }} />
+    <a className = {"item"} target = {"_blank"} href = {items[window.history.state.current][props.index].address} style = {{ "textDecoration": "none", "color": "black" }}>
+      <img src = {"https://www.google.com/s2/favicons?domain=" + items[window.history.state.current][props.index].address} style = {{ "height": "16px", "width": "16px" }} />
       <div style = {{ "fontWeight": "bold" }}>
-        {items[parent][props.index].name}
+        {items[window.history.state.current][props.index].name}
       </div>
       <div>
-        {items[parent][props.index].description}
+        {items[window.history.state.current][props.index].description}
       </div>
     </a>
     <button onClick = {function() {
@@ -74,41 +75,44 @@ function Folder(props) {
   if (editing[0]) {
     return <form className = {"item"} onSubmit = {function(event) {
       event.preventDefault()
-      name.current.value = name.current.value.replaceAll(/[/?]/g, "")
-      for (let index in items[parent]) {
-        if (items[parent][index].name === name.current.value) {
-          alert("Folder names must be unique.")
-          name.current.focus()
-          return
+      name.current.value = name.current.value.replaceAll(/[/?\\]/g, "")
+      if (name.current.value) {
+        for (let index in items[window.history.state.current]) {
+          if (items[window.history.state.current][index].name === name.current.value && index !== props.index) {
+            alert("Folder names must be unique.")
+            name.current.focus()
+            return
+          }
         }
+        items[window.history.state.current][props.index].name = name.current.value
+        items[window.history.state.current][props.index].description = description.current.value
+        axios.put("/update", items[window.history.state.current][props.index])
+        editing[1]()
+      } else {
+        name.current.focus()
       }
-      items[parent][props.index].name = name.current.value
-      items[parent][props.index].description = description.current.value
-      axios.put("/update", items[parent][props.index])
-      editing[1]()
     }}>
-      <input ref = {name} defaultValue = {items[parent][props.index].name} placeholder = {"name"} />
-      <input ref = {description} defaultValue = {items[parent][props.index].description} placeholder = {"description"} />
+      <input ref = {name} defaultValue = {items[window.history.state.current][props.index].name} placeholder = {"name"} />
+      <input ref = {description} defaultValue = {items[window.history.state.current][props.index].description} placeholder = {"description"} />
       <button>
         done
       </button>
     </form>
   }
-  let path = window.location.pathname + "/" + items[parent][props.index].name
+  let path = window.location.pathname + "/" + items[window.history.state.current][props.index].name
   return <>
-    <a className = {"item"} href = {path} onClick = {function(event) {
-      event.preventDefault()
-      parent = items[parent][props.index].index
-      window.history.pushState(undefined, undefined, path)
+    <button className = {"folder"} onClick = {function() {
+      window.history.state.names[props.index] = items[window.history.state.current][props.index].name
+      window.history.pushState({ "names": window.history.state.names, "current": props.index }, undefined, path)
       render("Main")
-    }} style = {{ "textDecoration": "none", "color": "black" }}>
+    }}>
       <div style = {{ "fontWeight": "bold" }}>
-        {items[parent][props.index].name}
+        {items[window.history.state.current][props.index].name}
       </div>
       <div>
-        {items[parent][props.index].description}
+        {items[window.history.state.current][props.index].description}
       </div>
-    </a>
+    </button>
     <button onClick = {function() {
       editing[1](true)
     }} style = {{ "position": "absolute", "top": "16px", "right": "100px" }}>
@@ -117,22 +121,22 @@ function Folder(props) {
   </>
 }
 function Item(props) {
-  if (items[parent][props.index].address) {
+  if (items[window.history.state.current][props.index].address) {
     return <Bookmark index = {props.index} />
   }
   return <Folder index = {props.index} />
 }
-function Main() {
-  state.Main = react.useState()
+function Items() {
+  state.Items = react.useState()
   let folder = []
-  for (let index in items[parent]) {
+  for (let index in items[window.history.state.current]) {
     folder.push(<div key = {index} style = {{ "position": "relative" }}>
       <Item index = {index} />
       <button onClick = {function() {
         destroyed = []
-        destroy(parent, index)
+        destroy(window.history.state.current, index)
         axios.put("/destroy", { "user": user, "destroyed": destroyed })
-        render("Main")
+        render("Items")
       }} style = {{ "position": "absolute", "top": "16px", "right": "16px" }}>
         delete
       </button>
@@ -154,15 +158,15 @@ function Add() {
       if (!(bookmarkAddress.current.value.startsWith("https://") || bookmarkAddress.current.value.startsWith("http://"))) {
         bookmarkAddress.current.value = "https://" + bookmarkAddress.current.value
       }
-      items[parent][next] = { "user": user, "index": next, "parent": parent, "name": bookmarkName.current.value, "description": bookmarkDescription.current.value, "address": bookmarkAddress.current.value }
-      axios.post("/item", items[parent][next++])
+      items[window.history.state.current][++next] = { "user": user, "index": next, "parent": window.history.state.current, "name": bookmarkName.current.value, "description": bookmarkDescription.current.value, "address": bookmarkAddress.current.value }
+      axios.post("/create", items[window.history.state.current][next])
       bookmarkAddress.current.blur()
       bookmarkName.current.blur()
       bookmarkDescription.current.blur()
       bookmarkAddress.current.value = ""
       bookmarkName.current.value = ""
       bookmarkDescription.current.value = ""
-      render("Main")
+      render("Items")
     }}>
       <input ref = {bookmarkAddress} placeholder = {"address"} />
       <input ref = {bookmarkName} placeholder = {"name"} />
@@ -173,22 +177,26 @@ function Add() {
     </form>
     <form className = {"item"} onSubmit = {function(event) {
       event.preventDefault()
-      folderName.current.value = folderName.current.value.replaceAll(/[/?]/g, "")
-      for (let index in items[parent]) {
-        if (items[parent][index].name === folderName.current.value) {
-          alert("Folder names must be unique.")
-          folderName.current.focus()
-          return
+      folderName.current.value = folderName.current.value.replaceAll(/[/?\\]/g, "")
+      if (folderName.current.value) {
+        for (let index in items[window.history.state.current]) {
+          if (items[window.history.state.current][index].name === folderName.current.value) {
+            alert("Folder names must be unique.")
+            folderName.current.focus()
+            return
+          }
         }
+        items[window.history.state.current][++next] = { "user": user, "index": next, "parent": window.history.state.current, "name": folderName.current.value, "description": folderDescription.current.value }
+        items[next] = []
+        axios.post("/create", items[window.history.state.current][next])
+        folderName.current.blur()
+        folderDescription.current.blur()
+        folderName.current.value = ""
+        folderDescription.current.value = ""
+        render("Items")
+      } else {
+        folderName.current.focus()
       }
-      items[parent][next] = { "user": user, "index": next, "parent": parent, "name": folderName.current.value, "description": folderDescription.current.value }
-      items[next] = []
-      axios.post("/item", items[parent][next++])
-      folderName.current.blur()
-      folderDescription.current.blur()
-      folderName.current.value = ""
-      folderDescription.current.value = ""
-      render("Main")
     }}>
       <input ref = {folderName} placeholder = {"name"} />
       <input ref = {folderDescription} placeholder = {"description"} />
@@ -198,13 +206,39 @@ function Add() {
     </form>
   </>
 }
-axios.get("/item/" + user).then(function(response) {
+function Main() {
+  state.Main = react.useState()
+  let path = []
+  for (let index in window.history.state.names) {
+    path[index] = <button key = {index} onClick = {function() {
+      let names = window.history.state.names.slice(0, Number(index) + 1)
+      let path = ""
+      for (let index in names) {
+        path += "/" + names[index]
+      }
+      window.history.pushState({ "names": names, "current": index }, undefined, path)
+      render("Main")
+    }}>
+      {window.history.state.names[index]}
+    </button>
+  }
+  path[window.history.state.current] = window.history.state.names[window.history.state.current]
+  return <>
+    {path}
+    <div style = {{ "borderStyle": "solid" }}>
+      <Items />
+      <Add />
+    </div>
+  </>
+}
+axios.post("/findAll", { "user": user }).then(function(response) {
+  window.history.replaceState({ "names": [user], "current": "0" }, undefined, "/" + user)
   for (let index in response.data) {
     map[response.data[index].index] = true
     if (!response.data[index].address) {
-      if (response.data[index].name === path[step] && response.data[index].parent === parent) {
-        redirect += "/" + response.data[index].name
-        parent = response.data[index].index
+      if (response.data[index].name === path[step] && response.data[index].parent === window.history.state.current) {
+        window.history.state.names[response.data[index].index] = response.data[index].name
+        window.history.replaceState({ "names": window.history.state.names, "current": response.data[index].index }, undefined, window.location.pathname + "/" + response.data[index].name)
         ++step
       }
       items[response.data[index].index] = []
@@ -212,9 +246,7 @@ axios.get("/item/" + user).then(function(response) {
     items[response.data[index].parent][response.data[index].index] = response.data[index]
   }
   next = map.length
-  window.history.replaceState(undefined, undefined, "/" + redirect)
   reactDOM.createRoot(document.querySelector("div")).render(<react.StrictMode>
     <Main />
-    <Add />
   </react.StrictMode>)
 })
